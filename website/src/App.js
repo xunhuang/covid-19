e93221e1d7f03e8c02b5b544b4bd14626a3b0755
Xun Huang <xhuang@gmail.com>
2020-03-09 22:18:25 -0700

    init hospital map

---
+++ website/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { GoogleMap, OverlayView, Marker, LoadScript } from '@react-google-maps/api'
+
+var Hospitals = require('./hospitals.json');
+
+console.log("*************");
+console.log(Hospitals.features);
+console.log("*************");
+
+const BasicMap = (props) => {
+  const center = {
+    lat: 44.58,
+    lng: -96.451580,
+  }
+
+  let hospitals = Hospitals.features.map(a => {
+    return <Marker position={{
+      lat: a.geometry.coordinates[1],
+      lng: a.geometry.coordinates[0],
+    }}
+      title={a.properties.NAME}
+      k />;
+  })
+
+
+  return <div className='map'>
+    <div className='map-container'>
+      <LoadScript
+        id="script-loader"
+        googleMapsApiKey="AIzaSyCc0PJublS1nreH_DpI7YbJs3-yW-BqxSk"
+      >
+        <GoogleMap
+          id='traffic-example'
+          mapContainerStyle={{
+            height: "100vh",
+            width: "100%"
+          }}
+          zoom={4}
+          center={center}
+        >
+          <Marker position={center} />
+          {hospitals}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  </div>;
+}
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          US Hospitals 1 2 3
+      </div>
+        <BasicMap />
+      </header>
+    </div>
+  );
+}
+
+export default App;
