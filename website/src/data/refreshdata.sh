a3db7a9e82e7d6a8b791aa3ba5a358afa564e5a9
Xun Huang <xhuang@gmail.com>
2020-03-12 22:28:46 -0700

    refresh data

---
+++ website/src/data/refreshdata.sh
@@ -0,0 +1,6 @@
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv?fbclid=IwAR0nfgjjrrxXL9nV1WXI6CdIwV5fLV_COn8GXMCMGHCfvKaPxsR-duvAxaE | csvtojson > time_series_19-covid-Confirmed.json
+
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv?fbclid=IwAR0nfgjjrrxXL9nV1WXI6CdIwV5fLV_COn8GXMCMGHCfvKaPxsR-duvAxaE | csvtojson > time_series_19-covid-Deaths.json
+
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv?fbclid=IwAR0nfgjjrrxXL9nV1WXI6CdIwV5fLV_COn8GXMCMGHCfvKaPxsR-duvAxaE | csvtojson > time_series_19-covid-Recovered.json
+
