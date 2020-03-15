dac93c394a22897c55e9efa987a5159dde0f6729
Xun Huang <xhuang@gmail.com>
2020-03-14 22:09:06 -0700

    fix refresh script

--- data/refresh.sh
+++ data/refresh.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 node import_1.3_cases.js > casesrawjs
-vim -s vimcommand casesrawjs
-node new.js > ../website/src/data/1.3cases.json
-# rm -rf casesrawjs new.js
+cat casesrawjs | sed -e $'s/,{/,\\\n{/g'  > a1.js
+vim -s vimcommand a1.js
+cat new.js | tr -d '\r\n' >newoneline.js
+node newoneline.js > ../website/src/data/1.3cases.json
+rm -rf casesrawjs new.js
 node export.js
