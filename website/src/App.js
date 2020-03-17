3d03f93ba0e1899c86f9d0e720d5065b38253a89
Xun Huang <xhuang@gmail.com>
2020-03-16 21:21:38 -0700

    provide links

--- website/src/App.js
+++ website/src/App.js
@@ -554,17 +554,31 @@ function App() {
           county={county}
           state={state}
         />
+        <div>
+          <h4> Data Sources </h4>
+          <li>
+            <a target="_blank" href="https://github.com/CSSEGISandData/COVID-19">
+              Johns Hopkins CSSE
+          </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://coronavirus.1point3acres.com/en">
+              1point3acres.com
+          </a>
+          </li>
+
+          <li>
+            <a target="_blank" href="https://en.wikipedia.org/wiki/User:Michael_J/County_table">
+              Wikipedia county info
+          </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://hifld-geoplatform.opendata.arcgis.com/search?groupIds=2900322cc0b14948a74dca886b7d7cfc">
+              Homeland Infrastructure Foundation-Level Data (HIFLD)
+           </a>
+          </li>
+        </div>
       </header>
-      <div>
-        <h4> Data Sources </h4>
-        <li> Johns Hopkins CSSE:  https://github.com/CSSEGISandData/COVID-19 </li>
-        <li> https://coronavirus.1point3acres.com/en </li>
-        <li> https://en.wikipedia.org/wiki/User:Michael_J/County_table </li>
-        <li>  Homeland Infrastructure Foundation-Level Data (HIFLD)
-          https://hifld-geoplatform.opendata.arcgis.com/search?groupIds=2900322cc0b14948a74dca886b7d7cfc</li>
-
-
-      </div>
     </div>
   );
 }
