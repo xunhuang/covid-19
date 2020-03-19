c11a99feeb4ea0ae2cea27f1eee6cb2a415e562d
Xun Huang <xhuang@gmail.com>
2020-03-18 22:47:58 -0700

    spliting out new cases

--- website/src/App.js
+++ website/src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api'
-import { ResponsiveContainer, LineChart, Line, YAxis, XAxis, Tooltip, CartesianGrid, Legend } from 'recharts';
 import { makeStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import { countyModuleInit, lookupCountyInfo } from "./USCountyInfo.js";
 import * as USCounty from "./USCountyInfo.js";
 import Select from 'react-select';
@@ -17,7 +14,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { NearbyCounties, CountiesForStateWidget, AllStatesListWidget } from "./CountyListRender.js"
-import { Typography } from '@material-ui/core';
+import { BasicGraphNewCases } from "./GraphNewCases.js"
 
 const useStyles = makeStyles(theme => ({
   row: {
@@ -60,9 +57,6 @@ const useStyles = makeStyles(theme => ({
   table: {
     width: "100%"
   },
-  customtooltip: {
-    backgroundColor: "#FFFFFF",
-  }
 }));
 
 var shortNumber = require('short-number');
@@ -249,130 +243,6 @@ const USCountyInfoWidget = withRouter((props) => {
     </div>
   </div >;
 });
-
-function countyFromNewCases(cases_data) {
-  let newcases = cases_data.reduce((m, c) => {
-    let a = m[c.fulldate];
-    if (!a) a = 0;
-    a += c.people_count;
-    m[c.fulldate] = a;
-    return m;
-  }, {});
-
-  const today = moment().format("MM/DD/YYYY");
-  var newcasenum = newcases[today];
-  if (!newcasenum) {
-    newcases[today] = 0;
-  }
-
-  let sorted_keys = Object.keys(newcases).sort(function (a, b) {
-    return moment(a, "MM/DD/YYY").toDate() - moment(b, "MM/DD/YYY").toDate();
-  });
-  let total = 0;
-  return sorted_keys.map(key => {
-    let v = newcases[key];
-    total += v;
-
-    const day = moment(key).format("M/D");
-
-    return {
-      name: day,
-      confirmed: total,
-      newcase: v,
-    };
-  });
-}
-
-const CustomTooltip = (props) => {
-  const classes = useStyles();
-  const { active } = props;
-  if (active) {
-    const today = moment().format("M/D");
-    const { payload, label } = props;
-    let confirmed;
-    let newcase;
-
-    payload.map(p => {
-      p = p.payload;
-      if ("confirmed" in p) {
-        confirmed = p.confirmed;
-      }
-      if ("pending_confirmed" in p) {
-        confirmed = p.pending_confirmed;
-      }
-      if ("newcase" in p) {
-        newcase = p.newcase;
-      }
-      if ("pending_newcase" in p) {
-        newcase = p.pending_newcase;
-      }
-    });
-
-    let pending_help;
-    if (today === payload[0].payload.name) {
-      pending_help = "Last # potentially incomplete";
-    }
-
-    return (
-      <div className={classes.customtooltip}>
-        <Typography variant="body1" noWrap>
-          {label}
-        </Typography>
-        <Typography variant="body2" noWrap>
-          {`Confirmed: ${confirmed}`}
-        </Typography>
-        <Typography variant="body2" noWrap>
-          {`New: ${newcase}`}
-        </Typography>
-        <Typography variant="body2" noWrap>
-          {pending_help}
-        </Typography>
-      </div>
-    );
-  }
-  return null;
-}
-
-
-const BasicGraphNewCases = (props) => {
-
-  let data = countyFromNewCases(props.casesData);
-
-  if (data.length > 2) {
-    let newdata = data.slice(0, data.length - 2);
-    let second_last = data[data.length - 2];
-    let last = data[data.length - 1];
-    second_last.pending_confirmed = second_last.confirmed;
-    second_last.pending_newcase = second_last.newcase;
-    let newlast = {
-      name: last.name,
-      pending_confirmed: last.confirmed,
-      pending_newcase: last.newcase,
-    };
-    newdata.push(second_last);
-    newdata.push(newlast);
-    data = newdata;
-  }
-
-  return <ResponsiveContainer height={300} >
-    <LineChart
-      data={data}
-      margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
-    >
-      <Tooltip content={<CustomTooltip />} />
-      <YAxis />
-      <XAxis dataKey="name" />
-      <CartesianGrid stroke="#f5f5f5" strokeDasharray="5 5" />
-      <Line type="monotone" dataKey="confirmed" stroke="#ff7300" yAxisId={0} strokeWidth={3} />
-      <Line type="monotone" dataKey="newcase" stroke="#387908" yAxisId={0} strokeWidth={3} />
-      <Line type="monotone" dataKey="pending_confirmed" stroke="#ff7300" strokeDasharray="1 1" strokeWidth={3} />
-      <Line type="monotone" dataKey="pending_newcase" stroke="#387908" strokeDasharray="1 1" strokeWidth={3} />
-      <Legend verticalAlign="top" payload={[
-        { value: 'Total', type: 'line', color: '#ff7300' },
-        { value: 'New', type: 'line', color: '#389708' },
-      ]} />
-    </LineChart></ResponsiveContainer>;
-}
 
 const Tag = (props) => {
   const classes = useStyles();
---
+++ website/src/GraphNewCases.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import { ResponsiveContainer, LineChart, Line, YAxis, XAxis, Tooltip, CartesianGrid, Legend } from 'recharts';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+
+const moment = require("moment");
+
+const useStyles = makeStyles(theme => ({
+    customtooltip: {
+        backgroundColor: "#FFFFFF",
+    }
+}));
+
+function countyFromNewCases(cases_data) {
+    let newcases = cases_data.reduce((m, c) => {
+        let a = m[c.fulldate];
+        if (!a) a = 0;
+        a += c.people_count;
+        m[c.fulldate] = a;
+        return m;
+    }, {});
+
+    const today = moment().format("MM/DD/YYYY");
+    var newcasenum = newcases[today];
+    if (!newcasenum) {
+        newcases[today] = 0;
+    }
+
+    let sorted_keys = Object.keys(newcases).sort(function (a, b) {
+        return moment(a, "MM/DD/YYY").toDate() - moment(b, "MM/DD/YYY").toDate();
+    });
+    let total = 0;
+    return sorted_keys.map(key => {
+        let v = newcases[key];
+        total += v;
+
+        const day = moment(key).format("M/D");
+
+        return {
+            name: day,
+            confirmed: total,
+            newcase: v,
+        };
+    });
+}
+
+const CustomTooltip = (props) => {
+    const classes = useStyles();
+    const { active } = props;
+    if (active) {
+        const today = moment().format("M/D");
+        const { payload, label } = props;
+        let confirmed;
+        let newcase;
+
+        payload.map(p => {
+            p = p.payload;
+            if ("confirmed" in p) {
+                confirmed = p.confirmed;
+            }
+            if ("pending_confirmed" in p) {
+                confirmed = p.pending_confirmed;
+            }
+            if ("newcase" in p) {
+                newcase = p.newcase;
+            }
+            if ("pending_newcase" in p) {
+                newcase = p.pending_newcase;
+            }
+        });
+
+        let pending_help;
+        if (today === payload[0].payload.name) {
+            pending_help = "Last # potentially incomplete";
+        }
+
+        return (
+            <div className={classes.customtooltip}>
+                <Typography variant="body1" noWrap>
+                    {label}
+                </Typography>
+                <Typography variant="body2" noWrap>
+                    {`Confirmed: ${confirmed}`}
+                </Typography>
+                <Typography variant="body2" noWrap>
+                    {`New: ${newcase}`}
+                </Typography>
+                <Typography variant="body2" noWrap>
+                    {pending_help}
+                </Typography>
+            </div>
+        );
+    }
+    return null;
+}
+
+const BasicGraphNewCases = (props) => {
+
+    let data = countyFromNewCases(props.casesData);
+
+    if (data.length > 2) {
+        let newdata = data.slice(0, data.length - 2);
+        let second_last = data[data.length - 2];
+        let last = data[data.length - 1];
+        second_last.pending_confirmed = second_last.confirmed;
+        second_last.pending_newcase = second_last.newcase;
+        let newlast = {
+            name: last.name,
+            pending_confirmed: last.confirmed,
+            pending_newcase: last.newcase,
+        };
+        newdata.push(second_last);
+        newdata.push(newlast);
+        data = newdata;
+    }
+
+    return <ResponsiveContainer height={300} >
+        <LineChart
+            data={data}
+            margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
+        >
+            <Tooltip content={<CustomTooltip />} />
+            <YAxis />
+            <XAxis dataKey="name" />
+            <CartesianGrid stroke="#f5f5f5" strokeDasharray="5 5" />
+            <Line type="monotone" dataKey="confirmed" stroke="#ff7300" yAxisId={0} strokeWidth={3} />
+            <Line type="monotone" dataKey="newcase" stroke="#387908" yAxisId={0} strokeWidth={3} />
+            <Line type="monotone" dataKey="pending_confirmed" stroke="#ff7300" strokeDasharray="1 1" strokeWidth={3} />
+            <Line type="monotone" dataKey="pending_newcase" stroke="#387908" strokeDasharray="1 1" strokeWidth={3} />
+            <Legend verticalAlign="top" payload={[
+                { value: 'Total', type: 'line', color: '#ff7300' },
+                { value: 'New', type: 'line', color: '#389708' },
+            ]} />
+        </LineChart></ResponsiveContainer>;
+}
+
+export { BasicGraphNewCases };
