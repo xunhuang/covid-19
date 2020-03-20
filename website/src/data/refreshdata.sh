a6f3845cc8555cc55133258beaa2b774e648d9ad
Xun Huang <xhuang@gmail.com>
2020-03-20 10:10:03 -0700

    refresh data

--- website/src/data/refreshdata.sh
+++ website/src/data/refreshdata.sh
@@ -1,6 +1,2 @@
-curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv?fbclid=IwAR0nfgjjrrxXL9nV1WXI6CdIwV5fLV_COn8GXMCMGHCfvKaPxsR-duvAxaE | csvtojson > time_series_19-covid-Confirmed.json
-
-curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv?fbclid=IwAR0nfgjjrrxXL9nV1WXI6CdIwV5fLV_COn8GXMCMGHCfvKaPxsR-duvAxaE | csvtojson > time_series_19-covid-Deaths.json
-
-curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv?fbclid=IwAR0nfgjjrrxXL9nV1WXI6CdIwV5fLV_COn8GXMCMGHCfvKaPxsR-duvAxaE | csvtojson > time_series_19-covid-Recovered.json
+curl https://covidtracking.com/api/states/daily |jq > state_testing.json 
 
