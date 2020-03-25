4775945f7fd80ecf0a1a1d6e82162a8b14d15bfc
Xun Huang <xhuang@gmail.com>
2020-03-25 12:44:19 -0700

    save data

--- website/src/data/caserefresh.sh
+++ website/src/data/caserefresh.sh
@@ -1,3 +1,3 @@
-curl https://static.usafacts.org/public/data/covid-19/covid_confirmed_usafacts.csv |csvtojson |jq  > covid_confirmed_usafacts.json
-curl https://static.usafacts.org/public/data/covid-19/covid_deaths_usafacts.csv |csvtojson |jq  > covid_death_usafacts.json
+#curl https://static.usafacts.org/public/data/covid-19/covid_confirmed_usafacts.csv |csvtojson |jq  > covid_confirmed_usafacts.json
+#curl https://static.usafacts.org/public/data/covid-19/covid_deaths_usafacts.csv |csvtojson |jq  > covid_death_usafacts.json
 curl "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases_US/FeatureServer/0/query?f=json&where=(Confirmed%20%3E%200)%20AND%20(1%3D1)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID%20ASC&resultOffset=0&resultRecordCount=7000&cacheHint=true&quantizationParameters=%7B%22mode%22%3A%22edit%22%7D" | jq > latest.json
