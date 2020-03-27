e09c853b1b257700ced2508f605f92bca243b733
Xun Huang <xhuang@gmail.com>
2020-03-26 17:48:07 -0700

    add API keys

--- website/setupFirebaseConfig.js
+++ website/setupFirebaseConfig.js
@@ -5,6 +5,11 @@ const fbcli = require('firebase-tools');
 const fs = require('fs');
 
 fbcli.setup.web().then(config => {
+  const geoapi_keyfile_location = '/Users/xhuang/APIKEYs/ipgeojson_api.json';
+  if (fs.existsSync(geoapi_keyfile_location)) {
+    const geokey = JSON.parse(fs.readFileSync(geoapi_keyfile_location));
+    config.ipgeolocation_key = geokey.ipgeolocation;
+  }
   fs.writeFileSync(
     'src/firebaseConfig.json',
     JSON.stringify(config),
