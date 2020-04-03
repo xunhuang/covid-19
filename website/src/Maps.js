d1ec1a3201601b7fa76cc4bf192bcb1f17a201b4
April Schleck <april.schleck@gmail.com>
2020-04-03 09:52:50 -0700

    Add support for touch events, Safari is still busted but not sure why.

--- website/src/Maps.js
+++ website/src/Maps.js
@@ -9,11 +9,11 @@ const useStyles = theme => ({
     height: '100vh', // for full screen page, not required
     overflow: 'hidden',
     position: 'relative',
-    touchAction: 'none',
     width: '100%', // for full screen page, not required
   },
   map: {
     height: 'auto',
+    touchAction: 'none',
     transformOrigin: 'top left',
     width: '100%',
     willChange: 'transform',
@@ -58,12 +58,14 @@ class RawPin extends React.Component {
     return (
       <div className={classes.pin} style={divStyle}>
         <header>{this.state.header || ''}</header>
-        <content>{this.state.content || ''}</content>
+        <section>{this.state.content || ''}</section>
       </div>);
   }
 }
 
 const Pin = withStyles(useStyles)(RawPin);
+
+const MAX_MAP_ZOOM = 4;
 
 class RawMap extends React.Component {
   constructor(props) {
@@ -71,7 +73,7 @@ class RawMap extends React.Component {
     this.container = React.createRef();
     this.element = React.createRef();
     this.pin = React.createRef();
-    this.mouseState = undefined;
+    this.activeGesture = undefined;
     this.pinCenter = undefined;
   }
 
@@ -89,9 +91,6 @@ class RawMap extends React.Component {
             ref={this.container}>
           <SVG
               onLoad={this.svgLoaded_}
-              onMouseDown={this.mouseDown_}
-              onMouseMove={this.mouseMove_}
-              onMouseUp={this.mouseUp_}
               innerRef={this.element}
               className={classes.map}
               src={process.env.PUBLIC_URL + '/us_counties_map.svg'} />
@@ -110,8 +109,20 @@ class RawMap extends React.Component {
     this.renderColors_();
 
     // React doesn't support passive events yet
+    for (const [name, fn] of [
+          ['mousedown', this.mouseDown_],
+          ['mousemove', this.mouseMove_],
+          ['mouseup', this.mouseUp_],
+          ['touchstart', this.touchStart_],
+          ['touchmove', this.touchMove_],
+          ['touchend', this.touchEnd_],
+          ['wheel', this.wheel_],
+        ]) {
+      this.element.current.addEventListener(
+          name, fn.bind(this), {passive: false});
+    }
     this.element.current.addEventListener(
-        'wheel', this.wheel_, {passive: false});
+          'touchcancel', (e) => {e.preventDefault();}, {passive: false});
   };
 
   renderColors_() {
@@ -140,44 +151,95 @@ class RawMap extends React.Component {
     });
   }
 
-  mouseDown_ = (e) => {
-    this.mouseState = {action: 'down', x: e.clientX, y: e.clientY};
-  };
+  mouseDown_(e) {
+    e.preventDefault();
 
-  mouseMove_ = (e) => { 
-    if (!this.mouseState) {
+    this.activeGesture = {name: 'select', center: {x: e.clientX, y: e.clientY}};
+  }
+
+  mouseMove_(e) {
+    e.preventDefault();
+
+    if (!this.activeGesture) {
       return true;
     }
 
-    const state = this.mouseState;
-    if (state.action === 'down') {
-      const at = {x: e.clientX, y: e.clientY};
-      if (distance(state, at) > 4) {
-        state.action = 'drag';
-      }
-    }
+    this.maybeDrag_({x: e.clientX, y: e.clientY});
+  }
 
-    if (state.action === 'drag') {
-      this.transform.x += e.clientX - state.x;
-      this.transform.y += e.clientY - state.y;
-      this.updateTransform_();
-      state.x = e.clientX;
-      state.y = e.clientY;
-      return false;
-    }
-  };
+  mouseUp_(e) {
+    e.preventDefault();
 
-  mouseUp_ = (e) => {
-    if (!this.mouseState) {
+    if (!this.activeGesture) {
       return true;
     }
 
-    const at = {x: e.clientX, y: e.clientY};
-    const state = this.mouseState;
-    if (state.action === 'down'
-        && distance(at, state) < 4
+    this.maybeClick_({x: e.clientX, y: e.clientY}, e.target);
+  };
+
+  touchStart_(e) {
+    e.preventDefault();
+
+    if (e.touches.length == 1) {
+      this.activeGesture = {
+        name: 'select',
+        center: touchCenter(e.touches),
+      };
+    } else if (e.touches.length == 2) {
+      this.activeGesture = {
+        name: 'pinch',
+        center: touchCenter(e.touches),
+        baseZoom: this.transform.zoom,
+        apart: touchDistance(e.touches.item(0), e.touches.item(1)),
+      };
+    } else if (e.touches.length > 2) {
+      // No idea
+      this.activeGesture = undefined;
+    }
+  }
+
+  touchMove_(e) {
+    e.preventDefault();
+
+    if (!this.activeGesture) {
+      return;
+    }
+
+    if (e.touches.length == 1) {
+      const touch = e.touches.item(0);
+      this.maybeDrag_({x: touch.clientX, y: touch.clientY});
+    } else if (e.touches.length == 2) {
+      const apart = touchDistance(e.touches.item(0), e.touches.item(1));
+      const delta = (apart - this.activeGesture.apart) / this.activeGesture.apart;
+      this.zoom_(this.activeGesture.baseZoom + delta, this.activeGesture.center);
+    }
+  }
+
+  touchEnd_(e) {
+    e.preventDefault();
+
+    if (!this.activeGesture) {
+      return;
+    }
+
+    if (e.touches.length == 0) {
+      this.maybeClick_(touchCenter(e.changedTouches), e.target);
+    } else if (e.touches.length == 1) {
+      // End the pinch
+      this.activeGesture = undefined;
+    }
+  }
+
+  wheel_(e) {
+    e.preventDefault();
+    this.zoom_(this.transform.zoom + -0.01 * e.deltaY, {x: e.clientX, y: e.clientY});
+  }
+
+  maybeClick_(at, target) {
+    const gesture = this.activeGesture;
+    if (gesture.name === 'select'
+        && distance(at, gesture.center) < 4
         && this.props.getPinText) {
-      const target = e.target;
       if (target.hasAttribute('data-fips')) {
         const text = this.props.getPinText(
             target.getAttribute('data-fips').padStart(5, '0'));
@@ -196,16 +258,35 @@ class RawMap extends React.Component {
 
         this.updatePinTransform_();
       }
+
+      this.activeGesture = undefined;
+    }
+  }
+
+  maybeDrag_(at) {
+    const gesture = this.activeGesture;
+    if (gesture.name === 'select') {
+      if (distance(gesture.center, at) > 4) {
+        gesture.name = 'drag';
+      }
     }
 
-    this.mouseState = undefined;
-  };
+    if (gesture.name === 'drag' || gesture.name === 'pinch') {
+      this.transform.x += at.x - gesture.center.x;
+      this.transform.y += at.y - gesture.center.y;
+      this.updateTransform_();
+      gesture.center.x = at.x;
+      gesture.center.y = at.y;
+    }
+  }
 
-  wheel_ = (e) => {
-    e.preventDefault();
+  zoom_(zoom, at) {
+    if (zoom > MAX_MAP_ZOOM) {
+      return;
+    }
 
     const was = this.transform.zoom;
-    this.transform.zoom -= 0.01 * e.deltaY;
+    this.transform.zoom = zoom;
 
     // First, determine where our current center will be when we zoom.
     const cx = this.transform.x / was * this.transform.zoom;
@@ -215,8 +296,8 @@ class RawMap extends React.Component {
     // moved.
     const rect = this.container.current.getBoundingClientRect();
     const dz = this.transform.zoom - was;
-    const dx = -(e.clientX - rect.left) / this.transform.zoom * dz;
-    const dy = -(e.clientY - rect.top) / this.transform.zoom * dz;
+    const dx = -(at.x - rect.left) / this.transform.zoom * dz;
+    const dy = -(at.y - rect.top) / this.transform.zoom * dz;
 
     this.transform.x = cx + dx;
     this.transform.y = cy + dy;
@@ -252,7 +333,7 @@ class RawMap extends React.Component {
             this.transform.y,
             containerRect.height - elementRect.height,
             0);
-    this.transform.zoom = clamp(this.transform.zoom, 1, 4);
+    this.transform.zoom = clamp(this.transform.zoom, 1, MAX_MAP_ZOOM);
   }
 
   updateMapTransform_() {
@@ -326,6 +407,10 @@ class InfectionMap extends React.Component {
 }
 
 function clamp(x, l, h) {
+  if (h < l) {
+    return h;
+  }
+
   if (x < l) {
     return l;
   } else if (x > h) {
@@ -337,6 +422,21 @@ function clamp(x, l, h) {
 
 function distance(a, b) {
   return (a.x - b.x)**2 + (a.y - b.y)**2;
+}
+
+function touchCenter(touches) {
+  const center = {x: 0, y: 0};
+  for (let i = 0; i < touches.length; ++i) {
+    center.x += touches.item(i).clientX;
+    center.y += touches.item(i).clientY;
+  }
+  center.x /= touches.length;
+  center.y /= touches.length;
+  return center;
+}
+
+function touchDistance(a, b) {
+  return distance({x: a.clientX, y: a.clientY}, {x: b.clientX, y: b.clientY});
 }
 
 export { InfectionMap }
