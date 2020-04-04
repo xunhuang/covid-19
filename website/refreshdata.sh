f0f721660f22b039f501aade96d1476823bfef32
Xun Huang <xhuang@gmail.com>
2020-04-03 22:41:33 -0700

    start NYC collection

--- website/refreshdata.sh
+++ website/refreshdata.sh
@@ -1,10 +1,23 @@
 
 temp_file=$(mktemp)
 
+d=`date "+%m-%d-%Y"` 
+curl -s  "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/boro.csv" \
+   |perl -pe 's/\r//g' \
+   | ruby -rcsv -e 'puts CSV.parse(STDIN).transpose.map &:to_csv' \
+   | csvtojson  > $temp_file
+filesize=$(wc -c <"$temp_file")
+if  [ "$filesize" -ge "200" ]; then
+    echo "Updated NYC-BOROUGHS-$d.json ($filesize) "
+   mv $temp_file ../data/archive/NYC-BOROUGHS-$d.json
+else 
+    echo "File size $filesize too small"
+fi
+
 curl -s https://covidtracking.com/api/states/daily |jq >$temp_file
 filesize=$(wc -c <"$temp_file")
 if  [ "$filesize" -ge "100000" ]; then
-    echo "udpated state_testing.json ($filesize)"
+    echo "Updated state_testing.json ($filesize)"
     mv $temp_file src/data/state_testing.json 
 else 
     echo "file size $filesize too small"
@@ -13,7 +26,7 @@ fi
 curl -s https://covidtracking.com/api/us/daily |jq >$temp_file
 filesize=$(wc -c <"$temp_file")
 if  [ "$filesize" -ge "15000" ]; then
-    echo "udpated us_testing.json ($filesize) "
+    echo "Updated us_testing.json ($filesize) "
     mv $temp_file src/data/us_testing.json
 else 
     echo "file size $filesize too small"
@@ -22,21 +35,20 @@ fi
 curl -s https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv |csvtojson |jq '.[] | select (."Country/Region" == "US")' >  $temp_file
 filesize=$(wc -c <"$temp_file")
 if  [ "$filesize" -ge "1000" ]; then
-    echo "udpated us_recovery.json ($filesize) "
+    echo "Updated us_recovery.json ($filesize) "
     mv $temp_file src/data/us_recovery.json
 else 
     echo "file size $filesize too small"
 fi
 
-d=`date "+%m-%d-%Y"` 
+
 curl -s  "https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pub?output=csv&gid=1902046093&headers=false" | tail -n +5 | csvtojson >$temp_file
 filesize=$(wc -c <"$temp_file")
 if  [ "$filesize" -ge "10000" ]; then
-    echo "udpated BNO-$d.json ($filesize) "
+    echo "Updated BNO-$d.json ($filesize) "
    mv $temp_file ../data/archive/BNO-$d.json
 else 
     echo "file size $filesize too small"
 fi
-
 
 
