3ecf24b85b67002a5ca1bb7e3bb8518ae16f307e
Xun Huang <xhuang@gmail.com>
2020-04-21 17:56:59 -0700

    added what's new

--- website/src/Header.js
+++ website/src/Header.js
@@ -32,6 +32,7 @@ const Cookies = require("js-cookie");
 const moment = require("moment");
 
 const NewsData = require("./data/news.json");
+const WhatsNewData = require("./data/whatsnew.json");
 
 const useStyles = makeStyles(theme => ({
     SocialMediaRow: {
@@ -50,12 +51,14 @@ const useStyles = makeStyles(theme => ({
         margin: 2,
     },
     tagline: {
-        display: 'block',
+        display: 'flex',
         padding: 0,
         paddingTop: 10,
         paddingRight: 10,
         margin: 0,
         alignSelf: "flex-start",
+        alignItems: "center",
+        flexDirection: "column",
     },
     keepclam: {
         display: 'block',
@@ -98,20 +101,50 @@ const useStyles = makeStyles(theme => ({
         color: "white",
     },
     newsDate: {
-        margin: 4,
+        margin: 6,
     },
     newsTitle: {
         margin: 4,
     },
 }));
 
+const WhatsNewSection = (props) => {
+    const classes = useStyles();
+    const data =
+        WhatsNewData.sort((a, b) => moment(b.Date, "MM/DD/YYYY").toDate() - moment(a.Date, "MM/DD/YYYY").toDate())
+            .slice(0, 5);
+    return <div>
+        <SectionHeader>
+            <Typography variant="h5" noWrap>
+                What's New On Covid-19.Direct
+             </Typography>
+        </SectionHeader>
+        {data.map((item, i) =>
+            <Grid container wrap="nowrap">
+                <Grid item className={classes.newsDate}>
+                    <div>{moment(item.Date, "MM/DD/YYYY").format("M/D")}</div>
+                </Grid>
+                <Grid item className={classes.newsTitle}>
+                    <Typography variant="body1" noWrap>
+                        <a href={item.Link.length > 0 ? item.Link : null}> {item.Feature}</a>
+                    </Typography>
+
+                    <Typography variant="body2" noWrap>
+                        {item.commentary}
+                    </Typography>
+                </Grid>
+            </Grid >
+        )}
+    </div >;
+};
+
 const NewsSection = (props) => {
     const classes = useStyles();
     const data =
         NewsData.sort((a, b) => moment(b.Date, "MM/DD/YYYY").toDate() - moment(a.Date, "MM/DD/YYYY").toDate())
-            .slice(0, 5);
+            .slice(0, 10);
     return <div>
-        <SectionHeader>
+        <SectionHeader id="news">
             <Typography variant="h5" noWrap>
                 News
              </Typography>
@@ -140,6 +173,7 @@ const DonateButton = (props) => {
         </Typography>);
 }
 
+
 const SocialMediaButtons = (props) => {
     const classes = useStyles();
 
@@ -166,35 +200,46 @@ const Banner = withRouter((props) => {
     const history = useHistory();
     const classes = useStyles();
     const country = useContext(CountryContext);
+    const [showNews, setShowNews] = React.useState(false);
+
     let us_summary = country.summary();
     let url_shared =
         "https://covid-19.direct" +
         props.match.url +
         history.location.search;
     return (
-        <div className={classes.topContainer}>
-            <span className={classes.title}>
-                <Typography variant="h6" >
-                    COVID-19.direct
+        <div>
+            <div className={classes.topContainer}>
+                <span className={classes.title}>
+                    <Typography variant="h6" >
+                        COVID-19.direct
             </Typography>
-                <SocialMediaButtons
-                    url={url_shared}
-                    quote={quote}
-                />
-                <Typography variant="body2" noWrap>
-                    Updated: {moment(us_summary.generatedTime).format('lll')}
-                </Typography>
-            </span>
-            <span className={classes.grow}></span>
-            {/* <span className={classes.keepclam}> Keep Clam, #StayHome</span> */}
-            <span className={classes.tagline}>
-                <Typography variant="body1" >
-                    {/* #StayHome #StayInformed */}
+                    <SocialMediaButtons
+                        url={url_shared}
+                        quote={quote}
+                    />
+                    <Typography variant="body2" noWrap>
+                        Updated: {moment(us_summary.generatedTime).format('lll')}
+                    </Typography>
+                </span>
+                <span className={classes.grow}></span>
+                {/* <span className={classes.keepclam}> Keep Clam, #StayHome</span> */}
+                <span className={classes.tagline}>
+                    <Typography variant="body1" >
+                        {/* #StayHome #StayInformed */}
                         this too shall pass
             </Typography>
-                <DonateButton />
-            </span>
-        </div >);
+                    <DonateButton />
+                    <Typography variant="body1" onClick={() => setShowNews(!showNews)} >
+                        What's New?
+            </Typography>
+                </span>
+            </div >
+            {showNews &&
+                <WhatsNewSection />
+            }
+        </div >
+    );
 });
 
 const QPArea = (props) => {
