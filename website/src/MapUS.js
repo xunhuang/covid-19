eb6b6bb15bbfde22111f010c50e77bb8f9ca60da
Xun Huang <xhuang@gmail.com>
2020-04-24 12:36:50 -0700

    fix bugs with maps, minHeight and testcoverage navigation

--- website/src/MapUS.js
+++ website/src/MapUS.js
@@ -15,234 +15,240 @@ const logColors = () => {
 }
 
 const ColorScale = {
-    confirmed: logColors()
-        .domain([1, 200, 10000])
-        .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
-    confirmedPerMillion: logColors()
-        .domain([100, 1000, 10000])
-        .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
-    confirmedNew: logColors()
-        .domain([1, 200, 2000])
-        .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
-    confirmedNewPerMillion: logColors()
-        .domain([1, 200, 2000])
-        .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
-    death: logColors()
-        .domain([1, 100, 1000])
-        .range([NO_DATA_COLOR, "#5d99c6", "#002171"]),
-    deathPerMillion: logColors()
-        .domain([10, 100, 1000])
-        .range([NO_DATA_COLOR, "#5d99c6", "#002171"]),
-    timeToDouble: logColors()
-        .domain([1, 15, 300])
-        .range(["#004d40", "#4db6ac", NO_DATA_COLOR]),
-    tests: logColors()
-        .domain([10000, 650000])
-        .range([NO_DATA_COLOR, "#3f51b5", "#000051"]),
-    testsPerMillions: logColors()
-        .domain([7200, 33000])
-        .range([NO_DATA_COLOR, "#3f51b5", "#000051"]),
+  confirmed: logColors()
+    .domain([1, 200, 10000])
+    .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
+  confirmedPerMillion: logColors()
+    .domain([100, 1000, 10000])
+    .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
+  confirmedNew: logColors()
+    .domain([1, 200, 2000])
+    .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
+  confirmedNewPerMillion: logColors()
+    .domain([1, 200, 2000])
+    .range([NO_DATA_COLOR, "#f44336", "#2f0707"]),
+  death: logColors()
+    .domain([1, 100, 1000])
+    .range([NO_DATA_COLOR, "#5d99c6", "#002171"]),
+  deathPerMillion: logColors()
+    .domain([10, 100, 1000])
+    .range([NO_DATA_COLOR, "#5d99c6", "#002171"]),
+  timeToDouble: logColors()
+    .domain([1, 15, 300])
+    .range(["#004d40", "#4db6ac", NO_DATA_COLOR]),
+  tests: logColors()
+    .domain([10000, 650000])
+    .range([NO_DATA_COLOR, "#3f51b5", "#000051"]),
+  testsPerMillions: logColors()
+    .domain([7200, 33000])
+    .range([NO_DATA_COLOR, "#3f51b5", "#000051"]),
 }
 
 const CountyNavButtons = withRouter((props) => {
-    const county = props.county;
-    const state = county.state();
-    const metro = county.metro();
-    const history = props.history;
-    return <ToggleButtonGroup
-        value={null}
-        exclusive
-        size="large"
-        onChange={(e, route) => {
-            history.push(route);
-        }}
-    >
-        <ToggleButton size="small" value={county.routeTo()} >
-            {county.name}
-        </ToggleButton>
-        {metro &&
-            <ToggleButton value={metro.routeTo()} >
-                {metro.name} </ToggleButton>
-        }
-        <ToggleButton value={state.routeTo()} >
-            {state.name}</ToggleButton>
-    </ToggleButtonGroup>;
+  const county = props.county;
+  const state = county.state();
+  const metro = county.metro();
+  const history = props.history;
+  return <ToggleButtonGroup
+    value={null}
+    exclusive
+    size="large"
+    onChange={(e, route) => {
+      history.push(route);
+    }}
+  >
+    <ToggleButton size="small" value={county.routeTo()} >
+      {county.name}
+    </ToggleButton>
+    {metro &&
+      <ToggleButton value={metro.routeTo()} >
+        {metro.name} </ToggleButton>
+    }
+    <ToggleButton value={state.routeTo()} >
+      {state.name}</ToggleButton>
+  </ToggleButtonGroup>;
 });
 
 const MapUS = withRouter((props) => {
-    const source = props.source;
-    const [subtab, setAlignment] = React.useState(getURLParam(props.history.location.search, "detailed") ?? 'confirmed');
-    const [perCapita, setPerCapita] = React.useState(true);
-    const [selectedCounty, setSelectedCounty] = React.useState(null);
+  const source = props.source;
+  const [subtab, setAlignment] =
+    React.useState(getURLParam(props.history.location.search, "detailed") ?? 'confirmed');
+  const [perCapita, setPerCapita] = React.useState(true);
+  const [selectedCounty, setSelectedCounty] = React.useState(null);
 
-    const buttonGroup = <ToggleButtonGroup
-        value={subtab}
-        exclusive
-        size="small"
-        onChange={(e, newvalue) => {
-            setAlignment(newvalue)
-            pushChangeTo(props.history, "detailed", newvalue);
-        }}
-    >
-        <ToggleButton value="confirmed"> Confirmed </ToggleButton>
-        <ToggleButton value="confirmedNew"> New </ToggleButton>
-        <ToggleButton value="death"> Death </ToggleButton>
-        <ToggleButton value="daysToDouble"> Days to Double </ToggleButton>
-        {source instanceof Country &&
-            <ToggleButton value="testCoverage"> Tests</ToggleButton>
-        }
-    </ToggleButtonGroup>;
+  let tabvalue = subtab;
+  if (subtab === "testCoverage" && !(source instanceof Country)) {
+    tabvalue = "confirmed";
+  }
 
-    const MyMap = {
-        confirmed: MapUSConfirmed,
-        confirmedNew: MapUSConfirmedNew,
-        death: MapStateDeath,
-        daysToDouble: MapDaysToDouble,
-        testCoverage: MapUSTestCoverage,
-    };
-    const ChosenMap = MyMap[subtab];
+  const buttonGroup = <ToggleButtonGroup
+    value={tabvalue}
+    exclusive
+    size="small"
+    onChange={(e, newvalue) => {
+      setAlignment(newvalue)
+      pushChangeTo(props.history, "detailed", newvalue);
+    }}
+  >
+    <ToggleButton value="confirmed"> Confirmed </ToggleButton>
+    <ToggleButton value="confirmedNew"> New </ToggleButton>
+    <ToggleButton value="death"> Death </ToggleButton>
+    <ToggleButton value="daysToDouble"> Days to Double </ToggleButton>
+    {source instanceof Country &&
+      <ToggleButton value="testCoverage"> Tests</ToggleButton>
+    }
+  </ToggleButtonGroup>;
 
-    return <div>
-        {buttonGroup}
-        <Grid container alignItems="center">
-            <Grid item>
-                <AntSwitch checked={perCapita} onClick={() => { setPerCapita(!perCapita) }} />
+  const MyMap = {
+    confirmed: MapUSConfirmed,
+    confirmedNew: MapUSConfirmedNew,
+    death: MapStateDeath,
+    daysToDouble: MapDaysToDouble,
+    testCoverage: MapUSTestCoverage,
+  };
+  const ChosenMap = MyMap[subtab];
+
+  return <div>
+    {buttonGroup}
+    <Grid container alignItems="center">
+      <Grid item>
+        <AntSwitch checked={perCapita} onClick={() => { setPerCapita(!perCapita) }} />
+      </Grid>
+      <Grid item>
+        Per Capita
             </Grid>
-            <Grid item>
-                Per Capita
-            </Grid>
-        </Grid>
-        <ChosenMap {...props} source={source} perCapita={perCapita} selectionCallback={setSelectedCounty} />
-        {
-            selectedCounty &&
-            <CountyNavButtons county={selectedCounty} />
-        }
+    </Grid>
+    <ChosenMap {...props} source={source} perCapita={perCapita} selectionCallback={setSelectedCounty} />
+    {
+      selectedCounty &&
+      <CountyNavButtons county={selectedCounty} />
+    }
 
-    </div >
+  </div >
 });
 
 const MapDaysToDouble = React.memo((props) => {
-    return (
-        <MapCountyGeneric
-            {...props}
-            skipCapita={true}
-            getCountyDataPoint={(county) => {
-                return county.summary().daysToDouble;
-            }}
-            colorFunction={(data) => {
-                return ColorScale.timeToDouble(data);
-            }}
-            colorFunctionPerMillion={(data) => {
-                return ColorScale.timeToDouble(data);
-            }}
-            toolip={county => {
-                let days = county.summary().daysToDouble;
-                days = days ? days.toFixed(1) + " days" : "no data"
-                return `${county.name} Days to 2x: \n${days}`
-            }}
-        />
-    );
+  return (
+    <MapCountyGeneric
+      {...props}
+      skipCapita={true}
+      getCountyDataPoint={(county) => {
+        return county.summary().daysToDouble;
+      }}
+      colorFunction={(data) => {
+        return ColorScale.timeToDouble(data);
+      }}
+      colorFunctionPerMillion={(data) => {
+        return ColorScale.timeToDouble(data);
+      }}
+      toolip={county => {
+        let days = county.summary().daysToDouble;
+        days = days ? days.toFixed(1) + " days" : "no data"
+        return `${county.name} Days to 2x: \n${days}`
+      }}
+    />
+  );
 });
 
 const MapUSConfirmed = React.memo((props) => {
-    return (
-        <MapCountyGeneric
-            {...props}
-            getCountyDataPoint={(county) => {
-                return county.summary().confirmed;
-            }}
-            colorFunction={(data) => {
-                return ColorScale.confirmed(data);
-            }}
-            colorFunctionPerMillion={(data) => {
-                return ColorScale.confirmedPerMillion(data);
-            }}
-            toolip={county => {
-                return `${county.name}, Confirmed: ${county.summary().confirmed}, \n` +
-                    `Confirm/Mil: ${(county.summary().confirmed / county.population() * 1000000).toFixed(0)}`
-            }}
-        />
-    );
+  return (
+    <MapCountyGeneric
+      {...props}
+      getCountyDataPoint={(county) => {
+        return county.summary().confirmed;
+      }}
+      colorFunction={(data) => {
+        return ColorScale.confirmed(data);
+      }}
+      colorFunctionPerMillion={(data) => {
+        return ColorScale.confirmedPerMillion(data);
+      }}
+      toolip={county => {
+        return `${county.name}, Confirmed: ${county.summary().confirmed}, \n` +
+          `Confirm/Mil: ${(county.summary().confirmed / county.population() * 1000000).toFixed(0)}`
+      }}
+    />
+  );
 });
 
 const MapUSConfirmedNew = React.memo((props) => {
-    return (
-        <MapCountyGeneric
-            {...props}
-            getCountyDataPoint={(county) => {
-                return county.summary().newcases;
-            }}
-            colorFunction={(data) => {
-                return ColorScale.confirmedNew(data);
-            }}
-            colorFunctionPerMillion={(data) => {
-                return ColorScale.confirmedNewPerMillion(data);
-            }}
-            toolip={county => {
-                return `${county.name}, New: ${county.summary().newcases}, \n` +
-                    `New/Mil: ${(county.summary().newcases / county.population() * 1000000).toFixed(0)}`
-            }}
-        />
-    );
+  return (
+    <MapCountyGeneric
+      {...props}
+      getCountyDataPoint={(county) => {
+        return county.summary().newcases;
+      }}
+      colorFunction={(data) => {
+        return ColorScale.confirmedNew(data);
+      }}
+      colorFunctionPerMillion={(data) => {
+        return ColorScale.confirmedNewPerMillion(data);
+      }}
+      toolip={county => {
+        return `${county.name}, New: ${county.summary().newcases}, \n` +
+          `New/Mil: ${(county.summary().newcases / county.population() * 1000000).toFixed(0)}`
+      }}
+    />
+  );
 });
 
 const MapStateDeath = React.memo((props) => {
-    return (
-        <MapCountyGeneric
-            {...props}
-            getCountyDataPoint={(county) => {
-                return county.summary().deaths;
-            }}
-            colorFunction={(data) => {
-                return ColorScale.death(data);
-            }}
-            colorFunctionPerMillion={(data) => {
-                return ColorScale.deathPerMillion(data);
-            }}
-            toolip={county => {
-                return `${county.name}, Deaths: ${county.summary().deaths}, \n` +
-                    `Deaths/Mil: ${(county.summary().deaths / county.population() * 1000000).toFixed(0)}`
-            }}
-        />
-    );
+  return (
+    <MapCountyGeneric
+      {...props}
+      getCountyDataPoint={(county) => {
+        return county.summary().deaths;
+      }}
+      colorFunction={(data) => {
+        return ColorScale.death(data);
+      }}
+      colorFunctionPerMillion={(data) => {
+        return ColorScale.deathPerMillion(data);
+      }}
+      toolip={county => {
+        return `${county.name}, Deaths: ${county.summary().deaths}, \n` +
+          `Deaths/Mil: ${(county.summary().deaths / county.population() * 1000000).toFixed(0)}`
+      }}
+    />
+  );
 });
 
 const MapUSTestCoverage = React.memo((props) => {
-    return (
-        <MapStateGeneric
-            {...props}
-            getCountyDataPoint={(county) => {
-                return county.summary().tests;
-            }}
-            colorFunction={(data) => {
-                return ColorScale.tests(data);
-            }}
-            colorFunctionPerMillion={(data) => {
-                return ColorScale.testsPerMillions(data);
-            }}
-            toolip={county => {
-                return `${county.name}, Tests: ${county.summary().tests}, \n` +
-                    `Tests % : ${(county.summary().tests / county.population() * 100).toFixed(1)}%`
-            }}
-        />
-    );
+  return (
+    <MapStateGeneric
+      {...props}
+      getCountyDataPoint={(county) => {
+        return county.summary().tests;
+      }}
+      colorFunction={(data) => {
+        return ColorScale.tests(data);
+      }}
+      colorFunctionPerMillion={(data) => {
+        return ColorScale.testsPerMillions(data);
+      }}
+      toolip={county => {
+        return `${county.name}, Tests: ${county.summary().tests}, \n` +
+          `Tests % : ${(county.summary().tests / county.population() * 100).toFixed(1)}%`
+      }}
+    />
+  );
 });
 
 
 function getURLParam(url, key) {
-    const params = new URLSearchParams(url);
-    if (params.has(key)) {
-        return params.get(key);
-    } else {
-        return undefined;
-    }
+  const params = new URLSearchParams(url);
+  if (params.has(key)) {
+    return params.get(key);
+  } else {
+    return undefined;
+  }
 }
 
 function pushChangeTo(history, key, value) {
-    const params = new URLSearchParams(history.location.search);
-    params.set(key, value);
-    history.location.search = params.toString();
-    history.push(history.location)
+  const params = new URLSearchParams(history.location.search);
+  params.set(key, value);
+  history.location.search = params.toString();
+  history.push(history.location)
 }
 
 export { MapUS }
