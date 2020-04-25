51237600462f516ea1e7ab1bea2244b68e81a3c4
April Schleck <april.schleck@gmail.com>
2020-04-25 14:42:20 -0700

    Fix the header when showing metros spanning state lines. (#91)
    * Fix the header when showing metros spanning state lines.
    
    * Removing words helps in order to remove words

--- website/src/PageMetro.js
+++ website/src/PageMetro.js
@@ -4,20 +4,44 @@ import { ListCountiesForMetro } from "./CountyListRender.js"
 import { withHeader } from "./Header.js"
 import { MyTabs } from "./MyTabs.js"
 import { USInfoTopWidget } from './USInfoTopWidget.js'
-import * as Util from "./Util"
+import { getDefaultCounty } from "./Util"
 import { GraphSection } from "./graphs/Graphs"
 import { Title } from "./Title";
 
 const PageMetro = withHeader((props) => {
     const country = useContext(CountryContext);
-    const metro = country.metroForId(props.match.params.metro);
+    const metroByStates = country.metroByStatesForId(props.match.params.metro);
+
+    // The metro may span multiple states, so we have to see if the user's
+    // preferred county is a county in the metro and then figure out which metro
+    // object corresponds to the right state. Everything is awesome.
+    const def = getDefaultCounty();
+    // All the metro objects have all the counties, even if those counties are
+    // across state lines.
+    const any = metroByStates.values().next().value;
+    let county;
+    for (const candidate of any.allCounties()) {
+        if (def.county === candidate.name
+                && def.state === candidate.state().twoLetterName) {
+            county = candidate;
+            break;
+        }
+    }
+
+    // Can't find the preferred county, just pick the best one.
+    if (!county) {
+        county =
+            any
+                .allCounties()
+                .sort((a, b) => b.totalConfirmed() - a.totalConfirmed())[0];
+    }
+
+    const metro = county.metro();
     const state = metro.state();
-    const county = Util.getDefaultCountyForMetro(metro);
 
     const tabs = [
         <ListCountiesForMetro metro={metro} />,
     ];
-
 
     return (
         <>
