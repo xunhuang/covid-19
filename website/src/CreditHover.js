1a849509b3c336f12cca709dcdfb071da93c9624
McKay <bmckaym@gmail.com>
2020-04-28 18:28:40 -0400

    Fixing misspelling of file name

--- website/src/CreditHover.js
+++ website/src/CreditHover.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles';
-import contributers from './data/contributers.json'
+import contributers from './data/contributors.json'
 
 const useStyles = makeStyles(theme => ({
     typography: {
