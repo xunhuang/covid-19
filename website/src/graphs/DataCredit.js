b59f03c1a6eaede2bd4c5ba47bd451ca8a77ca94
April Schleck <april.schleck@gmail.com>
2020-05-08 11:42:07 -0700

    Add the Wikidata for world data to the credits and sort the list.

--- website/src/graphs/DataCredit.js
+++ website/src/graphs/DataCredit.js
@@ -2,59 +2,52 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography'
 
 const DataCreditWidget = (props) => {
+  const sources = [{
+      label: "Johns Hopkins CSSE",
+      url: "https://github.com/CSSEGISandData/COVID-19",
+    }, {
+      label: "Wikipedia county info",
+      url: "https://en.wikipedia.org/wiki/User:Michael_J/County_table",
+    }, {
+      label: "Homeland Infrastructure Foundation-Level Data (HIFLD)",
+      url: "https://hifld-geoplatform.opendata.arcgis.com/search?groupIds=2900322cc0b14948a74dca886b7d7cfc",
+    }, {
+      label: "Covid tracking API",
+      url: "https://covidtracking.com/api/",
+    }, {
+      label: "Wikidata query for country and region data",
+      url: "https://w.wiki/QJc",
+    }, {
+      label: "Wikidata query for stay-at-home orders",
+      url: "https://w.wiki/LeZ",
+    }, {
+      label: "BNO News",
+      url: "https://bnonews.com/index.php/2020/04/the-latest-coronavirus-cases/",
+    }, {
+      label: "The Institute for Health Metrics and Evaluation",
+      url: "http://www.healthdata.org/covid/data-downloads",
+    }, {
+      label: "MSA definition from NLSInfo.org",
+      url: "https://www.nlsinfo.org/usersvc/NLSY97/NLSY97Rnd9geocodeCodebookSupplement/gatt101.htm",
+    }, {
+      label: "Local Health Departments from Naccho.org",
+      url: "https://www.naccho.org/membership/lhd-directory",
+    },
+  ];
+  sources.sort((a, b) => a.label < b.label ? -1 : 1);
+
   return (
     <div>
       <Typography variant="body1">
         We are incredibly grateful to people working hard to make their data available.
-            </Typography>
+      </Typography>
 
       <Typography variant="body2" >
-        <li>
-          <a target="_blank" href="https://github.com/CSSEGISandData/COVID-19" rel="noopener noreferrer" >
-            Johns Hopkins CSSE
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://en.wikipedia.org/wiki/User:Michael_J/County_table" rel="noopener noreferrer" >
-            Wikipedia county info
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://hifld-geoplatform.opendata.arcgis.com/search?groupIds=2900322cc0b14948a74dca886b7d7cfc" rel="noopener noreferrer" >
-            Homeland Infrastructure Foundation-Level Data (HIFLD)
-           </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://covidtracking.com/api/" rel="noopener noreferrer" >
-            Covid tracking API
-           </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://w.wiki/LeZ" rel="noopener noreferrer">
-            Wikidata query for stay-at-home orders
-                    </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://bnonews.com/index.php/2020/04/the-latest-coronavirus-cases/" rel="noopener noreferrer">
-            BNO News
-                    </a>
-        </li>
-        <li>
-          <a target="_blank" href="http://www.healthdata.org/covid/data-downloads" rel="noopener noreferrer">
-            The Institute for Health Metrics and Evaluation
-            </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://www.nlsinfo.org/usersvc/NLSY97/NLSY97Rnd9geocodeCodebookSupplement/gatt101.htm" rel="noopener noreferrer">
-            MSA definition from NLSInfo.org
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://www.naccho.org/membership/lhd-directory" rel="noopener noreferrer">
-            Local Health Departments from Naccho.org
-          </a>
-        </li>
-
+        {sources.map(({label, url}) => (
+          <li key={url}>
+            <a target="_blank" href={url} rel="noopener noreferrer">{label}</a>
+          </li>
+        ))}
       </Typography>
     </div>
   );
