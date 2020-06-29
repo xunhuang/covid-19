a9b8c15efb6dd930e5ea840814447f3f3b012629
Xun Huang <xhuang@gmail.com>
2020-06-28 23:56:42 -0700

    make the test use new nav component

--- website/src/graphs/GraphTestingEffort.js
+++ website/src/graphs/GraphTestingEffort.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
-    ResponsiveContainer, AreaChart, Area, YAxis, XAxis, Tooltip,
-    CartesianGrid, Legend, LineChart, Line
+  ResponsiveContainer, AreaChart, Area, YAxis, XAxis, Tooltip,
+  CartesianGrid, Legend, LineChart, Line
 } from 'recharts';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
@@ -9,236 +9,274 @@ import { Grid } from '@material-ui/core';
 import { AntSwitch } from "./AntSwitch.js"
 import { myShortNumber } from '../Util.js';
 import { mergeDataSeries, makeDataSeriesFromTotal, exportColumnFromDataSeries } from "./DataSeries";
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import { withRouter } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
-    customtooltip: {
-        backgroundColor: "#FFFFFF",
-    }
+  customtooltip: {
+    backgroundColor: "#FFFFFF",
+  }
 }));
 
 const CustomTooltip = (props) => {
-    const classes = useStyles();
-    const { active } = props;
-    if (active) {
-        const { payload, label } = props;
+  const classes = useStyles();
+  const { active } = props;
+  if (active) {
+    const { payload, label } = props;
+    let tested;
+    let positive;
+    let negative;
+    let totalPositve;
+    let totalTested;
+    let totalNegative;
+    let totalPending;
 
-        let tested;
-        let positive;
-        let negative;
-        let totalPositve;
-        let totalTested;
-        let totalNegative;
-        let totalPending;
+    payload.map(p => {
+      p = p.payload;
+      if ("testsThatDay" in p) {
+        tested = p.testsThatDay;
+      }
+      if ("positiveThatDay" in p) {
+        positive = p.positiveThatDay;
+      }
+      if ("negativeThatDay" in p) {
+        negative = p.negativeThatDay;
+      }
+      if ("positive" in p) {
+        totalPositve = p.positive;
+      }
+      if ("negative" in p) {
+        totalNegative = p.negative;
+      }
+      if ("pending" in p) {
+        totalPending = p.pending;
+      }
+      if ("total" in p) {
+        totalTested = p.total;
+      }
+      return null;
+    });
 
-        payload.map(p => {
-            p = p.payload;
-            if ("testsThatDay" in p) {
-                tested = p.testsThatDay;
-            }
-            if ("positiveThatDay" in p) {
-                positive = p.positiveThatDay;
-            }
-            if ("negativeThatDay" in p) {
-                negative = p.negativeThatDay;
-            }
-            if ("positive" in p) {
-                totalPositve = p.positive;
-            }
-            if ("negative" in p) {
-                totalNegative = p.negative;
-            }
-            if ("pending" in p) {
-                totalPending = p.pending;
-            }
-            if ("total" in p) {
-                totalTested = p.total;
-            }
-            return null;
-        });
-
-        return (
-            <div className={classes.customtooltip}>
-                <Typography variant="body1" noWrap>
-                    {label}
-                </Typography>
-                <Typography variant="body2" noWrap>
-                    {`Daily Tested: ${tested}`}
-                </Typography>
-                <Typography variant="body2" noWrap>
-                    {`Daily Positve : ${positive}`}
-                </Typography>
-                <Typography variant="body2" noWrap>
-                    {`Daily Negative : ${negative}`}
-                </Typography>
-                <Typography variant="body2" noWrap>
-                    {`Cumulative Positve Rate : ${(totalPositve / totalTested * 100).toFixed(1)} %`}
-                </Typography>
-                <Typography variant="body2" noWrap>
-                    {`Cumulative Negative Rate : ${(totalNegative / totalTested * 100).toFixed(1)} %`}
-                </Typography>
-                <Typography variant="body2" noWrap>
-                    {`Pending: ${totalPending ? totalPending : 0}`}
-                </Typography>
-            </div>
-        );
-    }
-    return null;
+    return (
+      <div className={classes.customtooltip}>
+        <Typography variant="body1" noWrap>
+          {label}
+        </Typography>
+        <Typography variant="body2" noWrap>
+          {`Daily Tested: ${tested}`}
+        </Typography>
+        <Typography variant="body2" noWrap>
+          {`Daily Positve : ${positive}`}
+        </Typography>
+        <Typography variant="body2" noWrap>
+          {`Daily Negative : ${negative}`}
+        </Typography>
+        <Typography variant="body2" noWrap>
+          {`Cumulative Positve Rate : ${(totalPositve / totalTested * 100).toFixed(1)} %`}
+        </Typography>
+        <Typography variant="body2" noWrap>
+          {`Cumulative Negative Rate : ${(totalNegative / totalTested * 100).toFixed(1)} %`}
+        </Typography>
+        <Typography variant="body2" noWrap>
+          {`Pending: ${totalPending ? totalPending : 0}`}
+        </Typography>
+      </div>
+    );
+  }
+  return null;
 }
 
 const formatYAxis = (tickItem) => {
-    return myShortNumber(tickItem);
+  return myShortNumber(tickItem);
 }
 
 function maybeFindTesting(source) {
-    let ancestor = source;
-    while (!ancestor.testing && ancestor.parent) {
-        ancestor = ancestor.parent();
-    }
-    return ancestor;
+  let ancestor = source;
+  while (!ancestor.testing && ancestor.parent) {
+    ancestor = ancestor.parent();
+  }
+  return ancestor;
 }
+const GraphTestingWidget = withRouter((props) => {
+  let subtabs = new Map([
+    ['effort', {
+      label: "Test Effort",
+      widget: <GraphTestingWidget1 {...props} />
+    }],
+    ['result', {
+      label: "Results",
+      widget: <GraphTestingWidget1 {...props} chart="result" />
+    }],
+  ]);
 
-const GraphTestingWidget = (props) => {
-    const [sourceData, setSourceData] = React.useState(null);
-    const [useAreaChart, setUseAreaChart] = React.useState(false);
+  let desired = getURLParam(props.history.location.search, "detailed");
+  if (!subtabs.has(desired)) {
+    desired = subtabs.keys().next().value;
+  }
+  const [subtab, setSubtab] = React.useState(desired);
 
-    React.useEffect(() => {
-        props.source.testingAsync()
-            .then(data => setSourceData(data));
-    }, [props.source])
+  const buttonGroup = <ToggleButtonGroup
+    value={subtab}
+    exclusive
+    size="small"
+    onChange={(e, newvalue) => {
+      setSubtab(newvalue)
+      pushChangeTo(props.history, "detailed", newvalue);
+    }}
+  >
+    {[...subtabs].map(([id, { label }]) =>
+      <ToggleButton key={id} value={id}>{label}</ToggleButton>
+    )}
+  </ToggleButtonGroup>;
 
-    if (!sourceData || sourceData.length === 0) {
-        return <div> Loading</div>;
-    }
+  return <div>
+    {buttonGroup}
+    {subtabs.get(desired).widget}
+  </div>;
+});
 
-    let data = sourceData.map(t => {
-        let md = t.date % 1000;
-        let m = Math.floor(md / 100);
-        let d = md % 100;
-        t.name = `${m}/${d}`;
+const GraphTestingWidget1 = (props) => {
+  const [sourceData, setSourceData] = React.useState(null);
+  const useAreaChart = props.chart === "result";
 
-        m = m.toString().padStart(2, "0");
-        d = d.toString().padStart(2, "0");
-        let y = Math.floor(t.date / 10000);
+  React.useEffect(() => {
+    props.source.testingAsync()
+      .then(data => setSourceData(data));
+  }, [props.source])
 
-        t.fulldate = `${m}/${d}/${y}`;
+  if (!sourceData || sourceData.length === 0) {
+    return <div> Loading</div>;
+  }
 
-        return t;
-    })
+  let data = sourceData.map(t => {
+    let md = t.date % 1000;
+    let m = Math.floor(md / 100);
+    let d = md % 100;
+    t.name = `${m}/${d}`;
 
-    data = data.sort(function (a, b) {
-        return a.date - b.date;
-    });
+    m = m.toString().padStart(2, "0");
+    d = d.toString().padStart(2, "0");
+    let y = Math.floor(t.date / 10000);
 
-    let testTotalArray = exportColumnFromDataSeries(data, "total");
-    let testPostives = exportColumnFromDataSeries(data, "positive");
-    let testNegatives = exportColumnFromDataSeries(data, "negative");
-    let total = makeDataSeriesFromTotal(testTotalArray, "total", "testsThatDay", "testsThatDay_avg");
-    let pos = makeDataSeriesFromTotal(testPostives, "postive", "positiveThatDay", "positiveThatDay_avg");
-    let neg = makeDataSeriesFromTotal(testNegatives, "negative", "negativeThatDay", "negativeThatDay_avg");
+    t.fulldate = `${m}/${d}/${y}`;
 
-    data = mergeDataSeries(data, total);
-    data = mergeDataSeries(data, pos);
-    data = mergeDataSeries(data, neg);
+    return t;
+  })
 
-    let total_tests = data.reduce((m, a) => { return a.total > m ? a.total : m }, 0);
-    let total_positives = data.reduce((m, a) => { return a.positive > m ? a.positive : m }, 0);
-    let total_negatives = data.reduce((m, a) => { return a.negative > m ? a.negative : m }, 0);
+  data = data.sort(function (a, b) {
+    return a.date - b.date;
+  });
 
-    // If true, show area chart.
-    // If false, show line chart.
-    let chart = useAreaChart ?
-        <AreaChart
-            data={data}
-            margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
-        >
-            <YAxis tickFormatter={formatYAxis} />
-            <XAxis dataKey="name" />
-            <CartesianGrid stroke="#d5d5d5" strokeDasharray="5 5" />
-            {/* <Line type="monotone" name="Total Tested" dataKey="total" stroke="#387908" yAxisId={0} strokeWidth={3} />
+  let testTotalArray = exportColumnFromDataSeries(data, "total");
+  let testPostives = exportColumnFromDataSeries(data, "positive");
+  let testNegatives = exportColumnFromDataSeries(data, "negative");
+  let total = makeDataSeriesFromTotal(testTotalArray, "total", "testsThatDay", "testsThatDay_avg");
+  let pos = makeDataSeriesFromTotal(testPostives, "postive", "positiveThatDay", "positiveThatDay_avg");
+  let neg = makeDataSeriesFromTotal(testNegatives, "negative", "negativeThatDay", "negativeThatDay_avg");
+
+  data = mergeDataSeries(data, total);
+  data = mergeDataSeries(data, pos);
+  data = mergeDataSeries(data, neg);
+
+  let total_tests = data.reduce((m, a) => { return a.total > m ? a.total : m }, 0);
+  let total_positives = data.reduce((m, a) => { return a.positive > m ? a.positive : m }, 0);
+  let total_negatives = data.reduce((m, a) => { return a.negative > m ? a.negative : m }, 0);
+
+  // If true, show area chart.
+  // If false, show line chart.
+  let chart = useAreaChart ?
+    <AreaChart
+      data={data}
+      margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
+    >
+      <YAxis tickFormatter={formatYAxis} />
+      <XAxis dataKey="name" />
+      <CartesianGrid stroke="#d5d5d5" strokeDasharray="5 5" />
+      {/* <Line type="monotone" name="Total Tested" dataKey="total" stroke="#387908" yAxisId={0} strokeWidth={3} />
         <Line type="monotone" name="Tested Positive" dataKey="positive" stroke="#ff7300" yAxisId={0} strokeWidth={3} /> */}
-            <Area stackId="1" type="monotone" name="Positive" dataKey="positiveThatDay" stroke="#ff7300" fill="#ff7300" yAxisId={0} strokeWidth={3} />
-            <Area stackId="1" type="monotone" name="Negative" dataKey="negativeThatDay" stroke="#00aeef" fill="#00aeef" yAxisId={0} strokeWidth={3} />
-            {/* <Area stackId="1" type="monotone" name="Pending" dataKey="pendingThatDay" stroke="#387908" fill="#387908" yAxisId={0} strokeWidth={3} /> */}
-            <Legend verticalAlign="top" />
-            <Tooltip content={<CustomTooltip />} />
-        </AreaChart>
-        :
-        <LineChart
-            data={data}
-            margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
-        >
-            <YAxis tickFormatter={formatYAxis} />
-            <XAxis dataKey="name" />
-            <CartesianGrid stroke="#d5d5d5" strokeDasharray="5 5" />
-            {/* <Line type="monotone" name="Total" dataKey="total" stroke="#ff7300" yAxisId={0} strokeWidth={3} /> */}
-            <Line type="monotone" name="Daily" dataKey="testsThatDay" dot={{ r: 1 }} strokeDasharray="2 2" stroke="#387908" yAxisId={0} strokeWidth={1} />
-            <Line type="monotone" name="3d daily avg" dataKey="testsThatDay_avg" dot={{ r: 1 }} stroke="#387908" yAxisId={0} strokeWidth={2} />
-            <Line type="monotone" name="Positive" dataKey="positiveThatDay" dot={{ r: 1 }} strokeDasharray="2 2" stroke="#a3a3a3" yAxisId={0} strokeWidth={1} />
-            <Line type="monotone" name="3d daily avg" dataKey="positiveThatDay_avg" dot={{ r: 1 }} stroke="#a3a3a3" yAxisId={0} strokeWidth={2} />
-            {/* <Line type="monotone" name="Negative" dataKey="negativeThatDay" stroke="#00aeef" yAxisId={0} strokeWidth={3} /> */}
-            <Legend verticalAlign="top" />
-            <Tooltip content={<CustomTooltip />} />
-        </LineChart>;
+      <Area stackId="1" type="monotone" name="Positive" dataKey="positiveThatDay" stroke="#ff7300" fill="#ff7300" yAxisId={0} strokeWidth={3} />
+      <Area stackId="1" type="monotone" name="Negative" dataKey="negativeThatDay" stroke="#00aeef" fill="#00aeef" yAxisId={0} strokeWidth={3} />
+      {/* <Area stackId="1" type="monotone" name="Pending" dataKey="pendingThatDay" stroke="#387908" fill="#387908" yAxisId={0} strokeWidth={3} /> */}
+      <Legend verticalAlign="top" />
+      <Tooltip content={<CustomTooltip />} />
+    </AreaChart>
+    :
+    <LineChart
+      data={data}
+      margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
+    >
+      <YAxis tickFormatter={formatYAxis} />
+      <XAxis dataKey="name" />
+      <CartesianGrid stroke="#d5d5d5" strokeDasharray="5 5" />
+      {/* <Line type="monotone" name="Total" dataKey="total" stroke="#ff7300" yAxisId={0} strokeWidth={3} /> */}
+      <Line type="monotone" name="Daily" dataKey="testsThatDay" dot={{ r: 1 }} strokeDasharray="2 2" stroke="#387908" yAxisId={0} strokeWidth={1} />
+      <Line type="monotone" name="3d daily avg" dataKey="testsThatDay_avg" dot={{ r: 1 }} stroke="#387908" yAxisId={0} strokeWidth={2} />
+      <Line type="monotone" name="Positive" dataKey="positiveThatDay" dot={{ r: 1 }} strokeDasharray="2 2" stroke="#a3a3a3" yAxisId={0} strokeWidth={1} />
+      <Line type="monotone" name="3d daily avg" dataKey="positiveThatDay_avg" dot={{ r: 1 }} stroke="#a3a3a3" yAxisId={0} strokeWidth={2} />
+      {/* <Line type="monotone" name="Negative" dataKey="negativeThatDay" stroke="#00aeef" yAxisId={0} strokeWidth={3} /> */}
+      <Legend verticalAlign="top" />
+      <Tooltip content={<CustomTooltip />} />
+    </LineChart>;
 
-    return <div>
-        <Grid container alignItems="center" justify="space-between" spacing={1}>
-            <Grid item container xs={12} sm={12} md={6} alignItems="center" justify="flex-start" spacing={1}>
-                <Grid item>
-                    <Typography variant="body2" noWrap>
-                        {`Total: ${myShortNumber(total_tests)}
+  return <div>
+    <Grid container alignItems="center" justify="space-between" spacing={1}>
+      <Grid item container xs={12} sm={12} md={6} alignItems="center" justify="flex-start" spacing={1}>
+        <Grid item>
+          <Typography variant="body2" noWrap>
+            {`Total: ${myShortNumber(total_tests)}
                     Pos.:${(total_positives / total_tests * 100).toFixed(0)}%
                     Neg.: ${(total_negatives / total_tests * 100).toFixed(0)}%
                     `}
-                    </Typography>
-                </Grid>
-                {/* Case 1: click on the "Line chart" text -> show line chart */}
-                <Grid item onClick={event => setUseAreaChart(false)}>
-                    <Typography>
-                    </Typography>
-                </Grid>
-                {/* Case 2: click on the ant switch -> change to the other chart type */}
-                <Grid item>
-                    <AntSwitch
-                        checked={useAreaChart}
-                        onClick={event => setUseAreaChart(!useAreaChart)}
-                    />
-                </Grid>
-                {/* Case 3: click on the "Area chart" text -> show area chart */}
-                <Grid item onClick={event => setUseAreaChart(true)}>
-                    <Typography>
-                        Results
-                    </Typography>
-                </Grid>
-            </Grid>
+          </Typography>
         </Grid>
-        <ResponsiveContainer height={300} >
-            {chart}
-        </ResponsiveContainer>
-        <Typography variant="body2" noWrap>
-            Data source: https://covidtracking.com/api/
+      </Grid>
+    </Grid>
+    <ResponsiveContainer height={300} >
+      {chart}
+    </ResponsiveContainer>
+    <Typography variant="body2" noWrap>
+      Data source: https://covidtracking.com/api/
         </Typography>
-    </div>;
+  </div>;
 }
 
 function maybeTestingTabFor(source) {
-    if (source.testingAsync) {
-        return {
-            id: 'testing',
-            label: 'Tests',
-            graph: GraphTestingWidget,
-        };
+  if (source.testingAsync) {
+    return {
+      id: 'testing',
+      label: 'Tests',
+      graph: GraphTestingWidget,
+    };
+  } else {
+    const maybeAncestor = maybeFindTesting(source);
+    if (maybeAncestor.testing) {
+      return {
+        id: 'testing',
+        label: `${maybeAncestor.name} Testing`,
+        graph: GraphTestingWidget,
+      };
     } else {
-        const maybeAncestor = maybeFindTesting(source);
-        if (maybeAncestor.testing) {
-            return {
-                id: 'testing',
-                label: `${maybeAncestor.name} Testing`,
-                graph: GraphTestingWidget,
-            };
-        } else {
-            return undefined;
-        }
+      return undefined;
     }
+  }
+}
+
+
+function getURLParam(url, key) {
+  const params = new URLSearchParams(url);
+  if (params.has(key)) {
+    return params.get(key);
+  } else {
+    return undefined;
+  }
+}
+
+function pushChangeTo(history, key, value) {
+  const params = new URLSearchParams(history.location.search);
+  params.set(key, value);
+  history.location.search = params.toString();
+  history.push(history.location)
 }
 
 export { maybeTestingTabFor };
