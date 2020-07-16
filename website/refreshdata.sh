f31bbe9da9e558eebdd6d7d93564dae3ac560dd9
Xun Huang <xhuang@gmail.com>
2020-07-16 14:58:37 -0700

    deleted old data

--- website/refreshdata.sh
+++ website/refreshdata.sh
@@ -1,73 +1,11 @@
 
-pushd ../COVID-19/ ; git checkout master; git pull; popd
-pushd  ../coronavirus-data/ ;  git checkout master; git pull; popd
+# pushd ../COVID-19/ ; git checkout master; git pull; popd
+# pushd  ../coronavirus-data/ ;  git checkout master; git pull; popd
 
 temp_file=$(mktemp)
+CSVTOJSON=./node_modules/.bin/csvtojson 
 
 d=`date "+%m-%d-%Y"` 
-
-fetchNYCDeath () {
-   curl -s "https://docs.google.com/spreadsheets/d/e/2PACX-1vTwQ7_6yDDF_pwfl8hNLXX-EX5IYNO_UrgbBDlr7MQXW70kE4kcl-CUNz_6e229lJK9GsgU6yRYuBqt/pub?gid=2045108071&single=true&output=csv" | tail -n +4 |head -6 |csvtojson |jq > $temp_file
-   filesize=$(wc -c <"$temp_file")
-   if  [ "$filesize" -ge "1000" ]; then
-       echo "Updated NYC-Deaths.json ($filesize) "
-       mv $temp_file ../data/archive/NYC-Deaths.json
-   else 
-       echo "file size $filesize too small"
-   fi
-}
-
-fetchUSTerritoriesConfirmed () {
-   curl -s  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRY5QQp-yHO9Lf9gye0211xxGM_oYiIOnrtIy89YJFS_tNm_MSrV5o2pT6w7MD4Jj6S-RTCCGyvn9as/pub?gid=0&single=true&output=csv" |csvtojson  |jq> $temp_file
-   filesize=$(wc -c <"$temp_file")
-   if  [ "$filesize" -ge "1000" ]; then
-       echo "Updated US-territories-confirm.json ($filesize) "
-       mv $temp_file ../data/archive/US-territories-confirmed.json
-   else 
-       echo "file size $filesize too small"
-   fi
-}
-
-fetchUSTerritoriesDeath () {
-   curl -s  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRY5QQp-yHO9Lf9gye0211xxGM_oYiIOnrtIy89YJFS_tNm_MSrV5o2pT6w7MD4Jj6S-RTCCGyvn9as/pub?gid=699244347&single=true&output=csv" |csvtojson  |jq> $temp_file
-   filesize=$(wc -c <"$temp_file")
-   if  [ "$filesize" -ge "1000" ]; then
-       echo "Updated US-territories-death.json ($filesize) "
-       mv $temp_file ../data/archive/US-territories-death.json
-   else 
-       echo "file size $filesize too small"
-   fi
-}
-
-fetchNYCDeath
-fetchUSTerritoriesConfirmed
-fetchUSTerritoriesDeath
-
-curl -s  "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/boro.csv" \
-   |perl -pe 's/\r//g' \
-   | ruby -rcsv -e 'puts CSV.parse(STDIN).transpose.map &:to_csv' \
-   | csvtojson  > $temp_file
-filesize=$(wc -c <"$temp_file")
-if  [ "$filesize" -ge "200" ]; then
-    echo "Updated NYC-BOROUGHS-$d.json ($filesize) "
-   mv $temp_file ../data/archive/NYC-BOROUGHS-$d.json
-   git add ../data/archive/NYC-BOROUGHS-$d.json
-else 
-    echo "File size $filesize too small"
-fi
-
-curl -s  "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/tests-by-zcta.csv" \
-   |perl -pe 's/\r//g' \
-   | ruby -rcsv -e 'puts CSV.parse(STDIN).transpose.map &:to_csv' \
-   | csvtojson  > $temp_file
-filesize=$(wc -c <"$temp_file")
-if  [ "$filesize" -ge "200" ]; then
-    echo "Updated NYC-ZIP-$d.json ($filesize) "
-   mv $temp_file ../data/archive/NYC-ZIP-$d.json
-   git add ../data/archive/NYC-ZIP-$d.json
-else 
-    echo "File size $filesize too small"
-fi
 
 curl -s https://covidtracking.com/api/states/daily |jq >$temp_file
 filesize=$(wc -c <"$temp_file")
@@ -87,7 +25,7 @@ else
     echo "file size $filesize too small"
 fi
 
-curl -s https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv |csvtojson |jq '.[] | select (."Country/Region" == "US")' >  $temp_file
+curl -s https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv |$CSVTOJSON |jq '.[] | select (."Country/Region" == "US")' >  $temp_file
 filesize=$(wc -c <"$temp_file")
 if  [ "$filesize" -ge "1000" ]; then
     echo "Updated us_recovery.json ($filesize) "
@@ -96,7 +34,7 @@ else
     echo "file size $filesize too small"
 fi
 
-curl -s  "https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pub?output=csv&gid=1902046093&headers=false" | tail -n +5 | csvtojson >$temp_file
+curl -s "https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pub?output=csv&gid=1902046093&headers=false" | tail -n +5 | $CSVTOJSON >$temp_file
 filesize=$(wc -c <"$temp_file")
 if  [ "$filesize" -ge "10000" ]; then
     echo "Updated BNO-$d.json ($filesize) "
@@ -105,35 +43,4 @@ if  [ "$filesize" -ge "10000" ]; then
 else 
     echo "file size $filesize too small"
 fi
-
-# curl -s "https://docs.google.com/spreadsheets/d/e/2PACX-1vT2INKOqGFCUpyx6MvrglF2ePvK_JqQofqc2frHos6acgNFXjO03JPVYUYZwEWMPLBms9PNfCUZ71Zw/pub?gid=0&single=true&output=csv" | csvtojson >$temp_file
-# filesize=$(wc -c <"$temp_file")
-# if  [ "$filesize" -ge "100" ]; then
-    # echo "Updated News.json ($filesize) "
-   # mv $temp_file ./src/data/news.json
-   # git add ./src/data/news.json
-# else 
-    # echo "file size $filesize too small"
-# fi
-
-# curl -s "https://docs.google.com/spreadsheets/d/e/2PACX-1vRb7RaaDwoqJhiPXvsFBWmERs-_xD7UdR477JKSr5G0pD8R1DnvfxulT-esUrNB1CMBFTjTGBctpvnw/pub?gid=0&single=true&output=csv" | csvtojson >$temp_file
-# filesize=$(wc -c <"$temp_file")
-# if  [ "$filesize" -ge "100" ]; then
-    # echo "Updated whatsnew.json ($filesize) "
-   # mv $temp_file ./src/data/whatsnew.json
-   # git add ./src/data/whatsnew.json
-# else
-    # echo "file size $filesize too small"
-# fi
-
-# curl -s "https://docs.google.com/spreadsheets/d/e/2PACX-1vTyCGNMkOWOyoUwIFCCZfNFizxYjikTxTxVSEt0t7sE-D-V0B-B7ZrGyZUuoYpjAl0Xf2geb9i_84be/pub?gid=0&single=true&output=csv" | csvtojson >$temp_file
-# filesize=$(wc -c <"$temp_file")
-# if  [ "$filesize" -ge "100" ]; then
-    # echo "Updated contribors.json ($filesize) "
-   # mv $temp_file ./src/data/contributors.json
-   # git add ./src/data/contributors.json
-# else 
-    # echo "file size $filesize too small"
-# fi
-
 
