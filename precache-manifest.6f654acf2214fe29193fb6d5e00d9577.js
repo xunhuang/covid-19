7e3b73cc94ffc301fdfef4c89018a4670b25ea52
xunhuang <xhuang@gmail.com>
2021-01-23 15:05:37 -0800

    Deploying to gh-pages from  @ 2fc4d4508d5a6a8c98bf179a01cc45e2b9907a7a 🚀

---
+++ precache-manifest.6f654acf2214fe29193fb6d5e00d9577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3062a844dda0b2cb6af908534fadacd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65d58390a1133738709d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a8eca4e5705a55ccd86",
+    "url": "/static/js/2.ede86d61.chunk.js"
+  },
+  {
+    "revision": "522b32ed6822dcb792a7a863c006276e",
+    "url": "/static/js/2.ede86d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d58390a1133738709d",
+    "url": "/static/js/main.2b8d42be.chunk.js"
+  },
+  {
+    "revision": "366ddaea945e197457a1",
+    "url": "/static/js/runtime-main.ad264d5c.js"
+  }
+]);
