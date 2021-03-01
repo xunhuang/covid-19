11a85dcd7dc7471e31975019521a8679dc665125
xunhuang <xunhuang@users.noreply.github.com>
2021-02-28 21:56:07 -0800

    Deploying to gh-pages from  @ 9f47c88acccb94d6de046ed0008a0efb5bc9dba1 🚀

---
+++ precache-manifest.9fd317213060dd423e8f9d75ac791407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab64d94d3bbec289b65210cb3f58cff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7bc87b99b41cb4da395",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a8451928b962894f723e",
+    "url": "/static/js/2.052d1f69.chunk.js"
+  },
+  {
+    "revision": "522b32ed6822dcb792a7a863c006276e",
+    "url": "/static/js/2.052d1f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bc87b99b41cb4da395",
+    "url": "/static/js/main.ba60b382.chunk.js"
+  },
+  {
+    "revision": "366ddaea945e197457a1",
+    "url": "/static/js/runtime-main.ad264d5c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2375fd51bfa79a8da663370e07e46972.js"
+  "/precache-manifest.9fd317213060dd423e8f9d75ac791407.js"
 );
 
 self.addEventListener('message', (event) => {
