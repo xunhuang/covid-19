be48bbc4319f210184c5e0e43e8d5e70295361a4
xunhuang <xunhuang@users.noreply.github.com>
2021-09-29 03:27:13 -0700

    Deploying to gh-pages-new from  @ 3e3a3c7e739465c9123f869bf499058da6e763c8 🚀

---
+++ precache-manifest.21ec4bfa898e1688721d0a8d77666e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e0859b5dbe51a8397ab3a9f3f570eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c221c938616f4e81091",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "12f78a5cc889506dcad7",
+    "url": "/static/js/2.b3e6385b.chunk.js"
+  },
+  {
+    "revision": "1242133120ee6ad99cda8971b036af02",
+    "url": "/static/js/2.b3e6385b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c221c938616f4e81091",
+    "url": "/static/js/main.d9101927.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
