89c540e4e739b7b9db801219651048e02987d5bc
xunhuang <xunhuang@users.noreply.github.com>
2021-10-13 14:54:50 -0700

    Deploying to gh-pages-new from  @ f96e5d229756bad7ca0710d1e5f849594e386ad7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39f0073369878387e805833802b33b72.js"
+  "/precache-manifest.868c6d132e6dcc839a67579491a00d6a.js"
 );
 
 self.addEventListener('message', (event) => {
